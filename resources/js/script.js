2a39ca16fbfc86e6997f1aea8178ab1a93256a23
Iqbal Hossan <engiqbal1999@gmail.com>
2022-02-17 15:45:23 +0600

    completed expenses and balance calculation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,27 +1,50 @@
+// get all input field value
 function getInputValue(input) {
     const inputField = document.getElementById(input + "-input");
-    const getInputFieldValue = inputField.value;
-    const getIntInputValue = parseFloat(getInputFieldValue);
+    const InputFieldValue = parseFloat(inputField.value);
+    // clear input field
     inputField.value = '';
 
-    return getIntInputValue
+    return InputFieldValue;
 }
 
+// get all total amounts innerText
+function getTotalAmount(total, amount) {
+    const totalAmount = document.getElementById(total + "-total");
+    let getPreviousTotal = parseFloat(totalAmount.innerText);
+    totalAmount.innerText = amount;
+
+}
+
+// update total expenses
+function updateTotalExpenses(food, rent, clothe) {
+    const totalExpenses = food + rent + clothe;
+    return totalExpenses;
+}
+
+// update total balance
+function updateBalance(income, totalCost) {
+    const totalBalance = income - totalCost;
+    return totalBalance;
+
+}
 // calculate between and expenses
 document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    // put all input id's
+    const income = getInputValue("income");
     const food = getInputValue("food");
     const rent = getInputValue("rent");
     const clothe = getInputValue("clothe");
 
     // total cost in a month
-    const totalExpenses = food + rent + clothe;
-    // console.log(totalCost);
+    const totalCost = updateTotalExpenses(food, rent, clothe);
+    const updateNewExpenses = getTotalAmount("expenses", totalCost);
 
-    const income = getInputValue("income");
-
-    const balance = income - totalExpenses;
-
-    console.log(balance);
+    // balance 
+    const balance = updateBalance(income, totalCost);
+    const updateNewBalance = getTotalAmount("balance", balance);
+    // getTotalAmount(balance);
 
 
 })
